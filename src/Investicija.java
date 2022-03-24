bc93aa58df087949ae403816fe755aa9c6f88686
joxxasasa <joxxasasa@gmail.com>
2022-03-24 16:51:14 +0100

    Jednostavni primeri

---
+++ src/Investicija.java
@@ -0,0 +1,67 @@
+
+public class Investicija {
+
+	public static void main(String[] args) {
+		/* Deklaracija promenljivih. */
+		/*double investicija; // Vrednost investicije.
+		double stopa; // Kamatana stopa.
+		double zarada;// Ukupna kamata po godini.
+		/* Izracunavanje. */
+		/*investicija = 1000;
+		stopa = 0.027;
+		zarada = investicija * stopa; // Compute the interest.
+		investicija = investicija + zarada;
+		// Izracunavanje investicije posle godinu dana sa kamatom.
+		// (Obratiti paznju da nova vrednost investicije prepisuje staru.)
+		/* Ispisivanje rezultata. */
+		/*System.out.print("Za godinu dana zaradjeno je: ");
+		System.out.print(zarada);
+		System.out.println(" dinara.");
+		System.out.print("Ukupna vrednost investicije je sada: ");
+		System.out.print(investicija);
+		System.out.print(" dinara.");*/
+		
+		/*double kursevra = 123.5;
+		int sumadinara = 155000;
+		double sumaevra;
+		sumaevra = sumadinara / kursevra;
+		System.out.print ("Pri promeni " + sumadinara);
+		System.out.print (" dinara, dobijamo " + sumaevra);
+		System.out.print (" evra po kursu od " + kursevra);
+		System.out.print (" dinara za evro.");*/
+		
+		/*double potrosenoGoriva = 17;
+		double predjenoKilometara = 350;
+		double prosecnaPotrosnja;
+		prosecnaPotrosnja = potrosenoGoriva / predjenoKilometara / 100;
+		System.out.print ("Prosecna potrosnja goriva je " + prosecnaPotrosnja);*/
+		
+		/*double poluprecnikKruga = 20;
+		double pi = 3.14;
+		double povrsinaKruga = poluprecnikKruga * poluprecnikKruga * pi;
+		double obimKruga = 2 * poluprecnikKruga * pi;
+		System.out.print("Ako je poluprecnik kruga 20 cm površina je " + povrsinaKruga);
+		System.out.print (" cm kvadratnih a obim" + obimKruga);*/
+		
+				
+		/* Deklaracija promenljivih. 
+		double investicija; // Vrednost investicije.
+		int stopa; // Kamatana stopa.
+		double // Ukupna kamata po godini.
+		/* Izracunavanje. */
+		/*investicija = 1000;
+		stopa = 0.027;
+		zarada = investicija * stopa; // Compute the interest.
+		investicija = investicija / zarada;
+		// Izracunavanje investicije posle godinu dana sa kamatom.
+		// (Obratiti paznju da nova vrednost investicije prepisuje staru.)
+		/* Ispisivanje rezultata. */
+		/*System.out.print("Za godinu dana zaradjeno je: ");
+		System.out.print(zarade);
+		System.out.println(" dinara.");
+		System.out.print("Ukupna vrednost investije je sada: ");
+		System.out.print(stopa);
+		System.out.print(" dinara.");*/
+		
+	}
+}
