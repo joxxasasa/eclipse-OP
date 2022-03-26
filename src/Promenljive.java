2a8be32cf3127018fbd3850aa43da25e46ecf8d3
joxxasasa <joxxasasa@gmail.com>
2022-03-26 09:22:38 +0100

    promenljive

---
+++ src/Promenljive.java
@@ -0,0 +1,18 @@
+
+public class Promenljive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b;
+		System.out.println("Vrednost promenljive a je:");
+		System.out.println(a);
+		b = a + 12;
+		System.out.println("Vrednost promenljive b=a+12 je:");
+		System.out.println(b);
+		a = a + 2;
+		System.out.println("Vrednost promenljive a=a+2 je:");
+		System.out.println(a);
+	}
+
+}
---
+++ src/konstantnePromenljive.java
@@ -0,0 +1,70 @@
+
+public class konstantnePromenljive {
+	public static void main(String[] args) {
+			/*int x = 3;
+			  
+			System.out.print("x je " + x);*/
+			
+			/*int x = 3;
+			  
+			System.out.print("x je " + x + " a novo x je " + (x+2));*/
+		
+			/*int x = 2;
+			int y = 4;
+			
+			System.out.print("x je " + x + ", y je " + y + " a njihov zbir je " + ( x + y));*/
+		
+			/*double x = 4;
+			 
+			System.out.print("x je " + x + " a vrednost izraza je " + (x*x + 1/x));*/
+		
+			//System.out.print("Pi je " + Math.PI + " a reciprocno Pi je " + 1/Math.PI);
+		
+			/*int x=2;
+			int b=4;
+			
+			System.out.println("x=" +x);
+			System.out.println("b=" +b);
+			System.out.println("(x+4*(b-6)=" +(x+4*(b-6)));*/
+		
+			/*int a = -1;
+			int b = 3;
+			
+			System.out.println("a=" + a);
+			System.out.println("b=" + b);
+			System.out.println("11+a*(b-6)=" + (11+a*(b-6)));*/
+		
+			/*int x = 1;
+			int y = 5;
+			
+			System.out.println("x=" + x);
+			System.out.println("y=" + y);
+			System.out.println("x/4+(2-y)=" + (x/4+(2-y)));*/
+		
+			//System.out.print(Integer.MAX_VALUE);
+		
+			/*double x = 1.357111;
+			int y = 5;
+			
+			System.out.println("x="+ x);
+			System.out.println("y="+ y);
+			
+			System.out.println("x="+ String.format("%.3g%n", x));
+			System.out.println("y="+ y);*/
+		
+			/*double x = 1;
+			double y = 0;
+			System.out.println("x= " + x);
+			System.out.println("y= " + y);
+			System.out.println("sin x = "  + (Math.sin(x)));	
+			System.out.println("cos y = "  + (Math.cos(y)));	
+			System.out.println("sin x + cos y = "  + (Math.sin(x) + Math.cos(y)));*/
+		
+			/*double x = 10;
+			double y = 20;
+				
+			System.out.println("ln(" + x +") - ln(" + y +") = " + (Math.log(x)-Math.log(y)));*/
+				
+	}		
+}
+	
